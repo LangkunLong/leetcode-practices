24024188ed30df6b7c0acab4650a409b5ec4af26
larry <langkunlong@gmail.com>
2024-12-02 15:23:32 -0500

    use a set to detect overlap

--- leetcode_easy/202_happy_number.py
+++ leetcode_easy/202_happy_number.py
@@ -3,15 +3,13 @@ class Solution:
         # add up the square of all its digits, but how do i know if its looping 
         if n == 1:
             return True
+        
+        # detect cycle using a set, if a number repeats, then its a cycle
+        cycle = set()
+        while n != 1 :
+            n = sum(int(num)**2 for num in str(n))
+            if n in cycle:
+                return False
+            cycle.add(n)
 
-
-        while n != 1 and len(str(n)) != 1:
-            str_int = str(n)
-            temp_num = 0
-            for char in str_int:
-                temp_num += (int(char) ** 2)
-            if temp_num == 1:
-                return True
-            print(temp_num)
-            n = temp_num
-        return False
+        return True
