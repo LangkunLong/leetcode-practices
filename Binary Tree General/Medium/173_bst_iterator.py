ed86a514e17958e7ec3715f85419718e5a0f3702
larry <langkunlong@gmail.com>
2025-07-02 22:37:16 -0400

    init

---
+++ Binary Tree General/Medium/173_bst_iterator.py
@@ -0,0 +1,10 @@
+class BSTIterator:
+
+    # assume we are given a BST
+    def __init__(self, root: Optional[TreeNode]):
+        
+
+    def next(self) -> int:
+        
+
+    def hasNext(self) -> bool:
