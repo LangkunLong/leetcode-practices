4d98d4eefb0ba1de2a5167b802d9c74b5f04549d
larry <langkunlong@gmail.com>
2025-07-02 22:45:28 -0400

    current workflow

--- Binary Tree General/Medium/173_bst_iterator.py
+++ Binary Tree General/Medium/173_bst_iterator.py
@@ -2,9 +2,19 @@ class BSTIterator:
 
     # assume we are given a BST
     def __init__(self, root: Optional[TreeNode]):
+        self.head = root
+        self.smallest = next()
         
-
+    # returns current smallest element
     def next(self) -> int:
+        smallest = None
+        while(self.head):
+            if not self.head:
+                return 
+            self.head = self.head.left
+            smallest = self.head.val
+            self.head = self.head.right
+        return smallest
         
-
+    # is there 
     def hasNext(self) -> bool:
