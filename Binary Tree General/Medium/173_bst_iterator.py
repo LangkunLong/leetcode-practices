11e2f91fe628d825cd9f976a4d11b0613a86d98e
larry <langkunlong@gmail.com>
2025-07-02 23:18:46 -0400

    solved with O(n) space, looking to reduce to O(h)

--- Binary Tree General/Medium/173_bst_iterator.py
+++ Binary Tree General/Medium/173_bst_iterator.py
@@ -1,20 +1,28 @@
 class BSTIterator:
+    from collections import deque
 
-    # assume we are given a BST
+    # assume we are given a BST, store everything inorder with a queue; O(1) removal
     def __init__(self, root: Optional[TreeNode]):
-        self.head = root
-        self.smallest = next()
+        self.queue = deque()
+        def helper(node):
+            if not node:
+                return
+            helper(node.left)
+            self.queue.append(node.val)
+            helper(node.right)
+        helper(root)
         
     # returns current smallest element
     def next(self) -> int:
-        smallest = None
-        while(self.head):
-            if not self.head:
-                return 
-            self.head = self.head.left
-            smallest = self.head.val
-            self.head = self.head.right
-        return smallest
+        return self.queue.popleft()
         
     # is there 
     def hasNext(self) -> bool:
+        return len(self.queue) != 0
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
