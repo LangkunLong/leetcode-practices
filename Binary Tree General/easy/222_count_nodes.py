1fa95396481f847e334158cbfb91de44855cf8ff
Larry Long <Langkunlong@gmail.com>
2025-07-04 16:21:42 -0400

    Update 222_count_nodes.py
    simple in-order traversal

--- Binary Tree General/easy/222_count_nodes.py
+++ Binary Tree General/easy/222_count_nodes.py
@@ -1,1 +1,13 @@
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        count = [0]
 
+        def helper(root):
+            if not root:
+                return 
+            helper(root.left)
+            count[0] += 1
+            helper(root.right)
+        
+        helper(root)
+        return count[0]
