b02e41d3d729255646871494cef8e40fe90b0bb6
Larry Long <Langkunlong@gmail.com>
2025-07-04 17:06:12 -0400

    Using brute force solution
    Currently finding all nodes between root and p, and root and q, and comparing the nodes, but there should be a faster way

--- Binary Tree General/Medium/236_lca_of_binary_tree.py
+++ Binary Tree General/Medium/236_lca_of_binary_tree.py
@@ -1,32 +1,50 @@
-def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         # by default root is the largest common ancestor
         # can we find any node that is a parent of p and q, including itself
         # what type traversal bfs or dfs? since we need the lowest level, dfs
         # ex: [3,5], [3,1], lca = 3
+
+        # brute force solution: find all path from root to p and q and find LCA
         if not root:
             return None
 
         lca_node = root
         p_nodes = self.helper(root, p)
         q_nodes = self.helper(root, q)
-        p_ptr, q_ptr = 0
+        p_ptr, q_ptr = 0,0
         while p_ptr in range(len(p_nodes)) and q_ptr in range(len(q_nodes)):
             if p_nodes[p_ptr].val != q_nodes[q_ptr].val:
                 break
             lca_node = p_nodes[p_ptr]
-            p_nodes += 1
-            q_ndoes += 1
+            p_ptr += 1
+            q_ptr += 1
         
         return lca_node
 
-    def helper(self, head, target, path_nodes):
+    def helper(self, head, target):
         path_nodes = []
-        while head:
-            if not head:
-                return 
-            path_nodes.append(head)
-            if head.val == target.val:
-                return 
-            self.helper(head.left, target)
-            self.helper(head.right, target)
+        found = [False]  # Use mutable to track if target is found
+
+        def recursion(cur):
+            if not cur or found[0]:
+                return
+            path_nodes.append(cur)
+            print(cur.val)
+            if cur.val == target.val:
+                found[0] = True
+                return
+            recursion(cur.left)
+            recursion(cur.right)
+            if not found[0]:
+                path_nodes.pop()  # if this path doesn't lead to target
+
+        recursion(head)
         return path_nodes
