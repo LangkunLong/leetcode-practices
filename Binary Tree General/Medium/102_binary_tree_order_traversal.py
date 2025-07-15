fa7fe5d3bc1e094a6f4aa1ca03cf552156867c94
Larry Long <Langkunlong@gmail.com>
2025-07-15 16:19:59 -0400

    using bfs with queue
    same solution as 637 + 199

--- Binary Tree General/Medium/102_binary_tree_order_traversal.py
+++ Binary Tree General/Medium/102_binary_tree_order_traversal.py
@@ -1,3 +1,24 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         # level order traversal same as bfs
+        if not root:
+            return []
+        
+        res = []
+        queue = collections.deque()
+        queue.append(root)
+
+        while(queue):
+            count = len(queue)
+            res_level = []
+            for i in range(count):
+                cur_node = queue.popleft()
+                res_level.append(cur_node.val)
+                
+                if cur_node.left:
+                    queue.append(cur_node.left)
+                if cur_node.right:
+                    queue.append(cur_node.right)
+            res.append(res_level)
+        
+        return res
