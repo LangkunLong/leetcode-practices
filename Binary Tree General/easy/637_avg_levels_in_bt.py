8eca7a379b5394b50d0be57cf455c9d627f7da02
Larry Long <Langkunlong@gmail.com>
2025-07-15 16:16:37 -0400

    Update 637_avg_levels_in_bt.py
    same approach as 199

--- Binary Tree General/easy/637_avg_levels_in_bt.py
+++ Binary Tree General/easy/637_avg_levels_in_bt.py
@@ -1,2 +1,22 @@
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        # use bfs, inserting and popping from the queue
+        # popleft all the elements in the current level, and append to the queue
+        res = []
+        queue = collections.deque()
+        queue.append(root)
+
+        while(queue):
+            count = len(queue)
+            res_level = []
+            for i in range(count):
+                cur_node = queue.popleft()
+                res_level.append(cur_node.val)
+                
+                if cur_node.left:
+                    queue.append(cur_node.left)
+                if cur_node.right:
+                    queue.append(cur_node.right)
+            res.append(sum(res_level) / len(res_level))
+        
+        return res
