9af60477d98d29aa4842d2377282684b7cd41454
Larry Long <Langkunlong@gmail.com>
2025-07-22 12:28:16 -0400

    using easier approach to reverse odd level nodes

--- Binary Tree General/Medium/103_binary_tree_zigzag_level_traversal.py
+++ Binary Tree General/Medium/103_binary_tree_zigzag_level_traversal.py
@@ -6,33 +6,24 @@
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # need to use level-order traversal, alternate between each level 
-        # queue can popleft and popright, alternate between popping order but maintain insertion order
+        # need to use level-order traversal, easy way to just reverse odd level nodes
         if not root:
             return []
         queue = collections.deque()
-        alt = True # so first pop after root is true
+        queue.append(root)
         res = []
         
-        queue.append(root)
         while(queue):
-            num_nodes = len(queue)
-            level_trav = []
-            for i in range(num_nodes):
-                cur_node = None
-                if alt:
-                    cur_node = queue.popleft()
-                else:
-                    cur_node = queue.pop()
-                if cur_node:
-                    level_trav.append(cur_node.val)
-                    if cur_node.left:
-                        queue.append(cur_node.left)
-                    if cur_node.right:
-                        queue.append(cur_node.right)
-            if not alt:
-                level_trav.reverse()
-            res.append(level_trav)
-            alt = not alt
+            level = []
+            for i in range(len(queue)):
+                cur_node = queue.popleft()
+                level.append(cur_node.val)
+                if cur_node.left:
+                    queue.append(cur_node.left)
+                if cur_node.right:
+                    queue.append(cur_node.right)
+            if len(res) % 2 == 1: # if level is odd, reverse
+                level = list(reversed(level))
+            res.append(level)
         
         return res
