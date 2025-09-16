0e43395ef160dd872f9debe1c850c3453c422c20
Larry Long <Langkunlong@gmail.com>
2025-09-15 20:43:38 -0400

    Add Trie data structure implementation

--- trie/208_implement_trie.py
+++ trie/208_implement_trie.py
@@ -25,6 +25,40 @@ class Trie:
         else:
             return False
 
+# using a linked-list / tree-like approach:
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.eow = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()
+            cur = cur.children[char] # update trie
+        cur.eow = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for char in word:
+            if char not in cur.children:
+                return False
+            cur = cur.children[char]
+        return cur.eow == True
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for char in prefix:
+            if char not in cur.children:
+                return False
+            cur = cur.children[char]
+        return True
+
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
