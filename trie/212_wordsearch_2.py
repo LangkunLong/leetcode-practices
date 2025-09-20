2730e4c92d4526b72f29f0f4fb169a3bd4be5a2d
Larry Long <Langkunlong@gmail.com>
2025-09-20 12:53:41 -0400

    need to prune trie after returning dfs
    Updated DFS implementation to prune the prefix tree during search.

--- trie/212_wordsearch_2.py
+++ trie/212_wordsearch_2.py
@@ -1,5 +1,3 @@
-# still failing tle after changing to in-grid marking
-
 class TrieNode:
     def __init__(self):
         self.children = dict()
@@ -9,6 +7,7 @@ class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         # step 1, reorganize words into a prefix trie
         # step 2, at each position on the grid, run recursive dfs to see how many words we can recreate
+        # HAVE TO PRUNE THE PREFIX TREE TO SOLVE OPTIMALLY
         root = TrieNode()
         for word in words:
             cur = root
@@ -31,16 +30,19 @@ class Solution:
                         return 
                     # backtracking: mark as visited in current path, then un-mark it when we return
                     cur_word += board[r][c]
-                    node = node.children[board[r][c]]
+                    child = node.children[ch]
                     board[r][c] = '#' # mark as visited
 
-                    if node.eow:
+                    if child.eow:
                         res.add(cur_word)
+                        # if marking the end of a trie branch, prune it so we don't need to traverse again
                     for dx, dy in neigh_dir:
                         nx, ny = r + dx, c + dy
                         if nx in range(len(board)) and ny in range(len(board[0])) and board[nx][ny] != '#':          
-                            dfs(r+dx, c+dy, node, cur_word)
+                            dfs(r+dx, c+dy, child, cur_word)
                     board[r][c] = ch
+                    if not child.children:
+                        del node.children[ch]
                 
                 dfs(i, j, root, "")
         
