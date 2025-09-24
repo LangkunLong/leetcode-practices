96dfd5215de232e0727b57e30043ecca82a25ed0
Larry Long <Langkunlong@gmail.com>
2025-09-24 14:06:15 -0400

    bit tricky, little bit of dp
    be mindful that the for loop will traverse all indexes, so it will start at all possible solutions containing 1 occurrence of each index.
    - no need for 2 recursive calls

--- backtracking/39_combination_sum.py
+++ backtracking/39_combination_sum.py
@@ -1,20 +1,25 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        from collections import Counter
+        # optimized approach is to define decision tree so that we single out each digit 
+        # 2 decision, either solution contain an occurrence of this digit, or do not contain 
+        # an occurrence of a digit
+
         res = []
-        def backtrack(sub_comb):
-            if sum(sub_comb) == target:
-                cur_count = Counter(sub_comb)
-                for exi_res in res:
-                    # solution exists
-                    if Counter(exi_res) == cur_count:
-                        return 
-                res.append(sub_comb)
-            if sum(sub_comb) > target:
-                return 
-            
-            for digit in candidates:
-                backtrack(sub_comb + [digit])
         
-        backtrack([])
+        def backtrack(sub_comb, cur_index, path):
+            # base case
+            if path == target:
+                res.append(sub_comb)
+                return 
+            if path > target:
+                return 
+            for start in range(cur_index, len(candidates)):
+                
+                # all possible solutions containing (adding) 1 occurrence of candidate[i]
+                # only need 1 recursion because the for loop will traverse all possible index
+                # next iteration of see all possible solutions containing (adding 1 occurrence)
+                # of candidate[i + 1]
+                backtrack(sub_comb + [candidates[start]], start, path + candidates[start])
+        
+        backtrack([], 0, 0)
         return res
