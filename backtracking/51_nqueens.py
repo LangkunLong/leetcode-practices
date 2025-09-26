9e1df27485b2e7d70ad8eeae58c6dc6def5272de
Larry Long <Langkunlong@gmail.com>
2025-09-26 17:30:46 -0400

    fixed bug for left traversal
    - keep the row the same, decrease column

--- backtracking/51_nqueens.py
+++ backtracking/51_nqueens.py
@@ -4,22 +4,24 @@ class Solution:
 
         # recursing over columns: 
         # for every column, try place a queen at every row, 
-        # if valid, we recurse and try to place the next queen at the next colum
+        # if valid, we recurse and try to place the next queen at the next column
         res = []
         def backtrack(col, board):
             # base case: if we reach index n, we filled column (n-1) so we filled all queens
+            
             if col == n:
                 res.append(["".join(row) for row in board]) # remember to deep copy
                 return 
             for row in range(n):
                 if queen_safe(row, col, board):
+                    print(board)
                     board[row][col] = 'Q'
                     backtrack(col + 1, board)
                     board[row][col] = '.' # BACKTRACK, have to remove the placed Queen
         
         # since we are placing column by column, only need to check the columns towards the left
         def queen_safe(row, col, board):
-            # diagonal
+            # up left diagonal
             cur_row, cur_col = row, col
             while cur_row in range(n) and cur_col in range(n):
                 if board[cur_row][cur_col] == 'Q':
@@ -27,19 +29,22 @@ class Solution:
                 cur_row -= 1
                 cur_col -= 1
             
+            # left
             cur_row = row
             cur_col = col
-            while cur_row in range(n):
-                if board[cur_row][cur_col] == 'Q':
-                    return False
-                cur_row -= 1
-            
-            cur_col = col
-            cur_row = row
             while cur_col in range(n):
                 if board[cur_row][cur_col] == 'Q':
                     return False
                 cur_col -= 1
+            
+            # left down diagonal
+            cur_col = col
+            cur_row = row
+            while cur_col in range(n) and cur_row in range(n):
+                if board[cur_row][cur_col] == 'Q':
+                    return False
+                cur_col -= 1
+                cur_row += 1
             return True
         
         board = [['.' for _ in range(n)] for _ in range(n)]
