4f7000e4f9b251b13cf9e5d59f0140123efa4f18
Larry Long <Langkunlong@gmail.com>
2025-09-28 19:02:02 -0400

    need to use dfs for sequence finding
    TLE error

--- backtracking/79_word_search.py
+++ backtracking/79_word_search.py
@@ -1,35 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         # run dfs only at positions that match starting index,
-        # or can i run a bfs?
         if not word or not board:
             return False
-        
-        from collections import deque
-        dirs = [(0,1), (0,-1), (-1,0), (1,0)]
+
         row, col = len(board), len(board[0])
-        
-        for r in range(len(board)):
-            for c in range(len(board[r])):
+        res = [False]
+        visited = set()
+
+        def backtrack(r, c, cur_word):
+            if r not in range(row) or c not in range(col) or (r,c) in visited:
+                return 
+            nxt_word = cur_word + board[r][c]
+            visited.add((r,c))
+            if nxt_word == word:
+                res[0] = True
+                return 
+            backtrack(r+1, c, nxt_word)
+            backtrack(r-1, c, nxt_word)
+            backtrack(r, c+1, nxt_word)
+            backtrack(r, c-1, nxt_word)
+            visited.remove((r,c))
+    
+        for r in range(row):
+            for c in range(col):
                 if board[r][c] == word[0]:
-                    # bfs
-                    queue = deque()
-                    visited = set()
-                    queue.append(((r,c), board[r][c]))
-                    while queue:
-                        cur_node = queue.popleft()
-                        cur_r, cur_c = cur_node[0]
-                        cur_word = cur_node[1]
-                        
-                        if cur_word == word:
-                            return True
-                        
-                        for dx, dy in dirs:
-                            nr, nc = cur_r + dx, cur_c + dy
-                            if nr in range(row) and nc in range(col) and (nr, nc) not in visited:
-                                queue.append(((nr,nc), cur_word + board[nr][nc]))
-                                visited.add((nr, nc))
-        return False
+                    backtrack(r,c,"")
+        return res[0]
                         
 
         
