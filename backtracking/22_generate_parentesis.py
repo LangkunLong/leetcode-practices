a05c4eba9fd52f0474c3445778802e5e58de70b7
Larry Long <Langkunlong@gmail.com>
2025-09-28 17:34:45 -0400

    optimized approach don't need stack to verify
    need to start with open parenthesis, and only add close parenthesis if we already have open ones

--- backtracking/22_generate_parentesis.py
+++ backtracking/22_generate_parentesis.py
@@ -1,46 +1,25 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         # decision tree is to append either an '(' or ')'
-        # base case: if len(sub_str) is 2n, check if valid, then we append solution
+        # optimized approach, can only add ')' if we have more open brackets
+        # open brackets and close brackets capped by n
+        # don't need to use a stack to verfiy because if we follow these rules the parenthesis
+        # will be valid
 
         res = []
-        def backtrack(cur_par):
-            if len(cur_par) == 2*n and valid(cur_par):
+        def backtrack(cur_par, n_open, n_close):
+            if n_open == n and n_close == n:
                 res.append("".join(cur_par))
                 return 
-            
-            for path in ['(', ')']:
-                cur_par.append(path)
-                if valid(cur_par):
-                    backtrack(cur_par)
+            if n_open < n :
+                cur_par.append('(')
+                backtrack(cur_par, n_open + 1, n_close)
                 cur_par.pop()
-        
-        
-        def valid(sub_sol):
-            if len(sub_sol) < 2*n:
-                n_open, n_close = 0,0
-                for par in sub_sol:
-                    if par == '(':
-                        n_open += 1
-                    else:
-                        n_close += 1
-                if n_open > n or n_close > n:
-                    return False
-            else:
-                stack = [] 
-                for par in sub_sol:
-                    if par == '(':
-                        stack.append(par)
-                    else:
-                        if stack:
-                            close = stack.pop()
-                            if close != '(':
-                                return False
-                        else:
-                            return False
-                if len(stack) != 0:
-                    return False
-            return True
-        backtrack([])
 
+            if n_close < n_open:
+                cur_par.append(')')
+                backtrack(cur_par, n_open, n_close + 1)
+                cur_par.pop()
+
+        backtrack([], 0, 0)
         return res
