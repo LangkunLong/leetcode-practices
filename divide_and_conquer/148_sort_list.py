67f11a14a4ca7988566d056cab20c8704549bf8f
Larry Long <Langkunlong@gmail.com>
2025-09-29 22:07:06 -0400

    attempt to use divide + conquer approach

--- divide_and_conquer/148_sort_list.py
+++ divide_and_conquer/148_sort_list.py
@@ -5,24 +5,45 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # using divide and conquer approach with merge sort
+        # dividing down to a single node, then merging them together
+        
+        # with trees, linked lists, base case is always if null node
         if not head:
             return None
         
-        cur = head
-        head_list = []
-        while cur:
-            head_list.append(cur.val)
-            cur = cur.next
-        head_list_sort = sorted(head_list)
-        #print(head_list_sort)
+        mid = self.getMid(head)
+        # need to separate into 2 linked lists
+        #print(mid.val)
+        right = mid.next
+        left = head
+        mid.next = None
 
-        def helper(l, r):
-            if l > r:
-                return None
-            node = ListNode(head_list_sort[l])
-            node.next = helper(l + 1, r)
-            return node
+        self.sortList(left) # left to mid node
+        self.sortList(right) # mid node + 1 to the end
+
+        # now have single node
+        merged_node = self.merge(left, right)
+        return merged_node
         
-        res = helper(0, len(head_list_sort)-1)
-    
-        return res
+    # helper to merge 2 sorted linked lists
+    def merge(self, left, right):
+        dummy = ListNode()
+        while left and right:
+            if left.val < right.val:
+                dummt.next = ListNode(left.val)
+                left = left.next
+            else:
+                dummy.next = ListNode(right.val)
+                right = right.next
+            dummy = dummy.next
+        return dummy.next
+
+
+    # slow and fast pointer helper to get mid in O(logn) time
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
