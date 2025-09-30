d03bdd3c5bf43ef35b143c99080897f149955cbc
Larry Long <Langkunlong@gmail.com>
2025-09-30 16:59:15 -0400

    using merge sort

--- divide_and_conquer/148_sort_list.py
+++ divide_and_conquer/148_sort_list.py
@@ -9,8 +9,11 @@ class Solution:
         # dividing down to a single node, then merging them together
         
         # with trees, linked lists, base case is always if null node
-        if not head:
-            return None
+        # also for merge sort, return when we reach a single base node, or else mid would return always the same node
+        # and we would keep calling sortList(left) and create an infinite loop
+        # we want to return the single node
+        if not head or not head.next:
+            return head
         
         mid = self.getMid(head)
         # need to separate into 2 linked lists
@@ -19,25 +22,32 @@ class Solution:
         left = head
         mid.next = None
 
-        self.sortList(left) # left to mid node
-        self.sortList(right) # mid node + 1 to the end
+        s_left = self.sortList(left) # left to mid node
+        s_right = self.sortList(right) # mid node + 1 to the end
 
         # now have single node
-        merged_node = self.merge(left, right)
+        merged_node = self.merge(s_left, s_right)
         return merged_node
         
     # helper to merge 2 sorted linked lists
     def merge(self, left, right):
         dummy = ListNode()
+        head = dummy
         while left and right:
             if left.val < right.val:
-                dummt.next = ListNode(left.val)
+                dummy.next = ListNode(left.val)
                 left = left.next
             else:
                 dummy.next = ListNode(right.val)
                 right = right.next
             dummy = dummy.next
-        return dummy.next
+        # if there is leftover from either list
+        if left:
+            dummy.next = left
+        if right:
+            dummy.next = right
+        
+        return head.next
 
 
     # slow and fast pointer helper to get mid in O(logn) time
