f172865c2b2dd4ec64dd62d56b9fb01fd889defc
Larry Long <Langkunlong@gmail.com>
2025-10-01 21:18:47 -0400

    find by using total value subtracting global minimum

--- kadane/918_max_sum_circular.py
+++ kadane/918_max_sum_circular.py
@@ -1,17 +1,33 @@
-
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        # pretty confusing question
+        # kadanane's algorithm, at each value do we set current array as the maximum subarray or pick the value itself
+        # for going around: maximum is just the total value removing the negative portion (total - global min)
+        # for noraml traversal: maximum is current max. (same as previous question)
+        
         if not nums:
             return 0
         
-        tot_sum = nums[0]
-        cur_sum = 0
-        for i in range(len(nums)):
-            if cur_sum + nums[i] >= 0:
-                cur_sum += nums[i]
-                tot_sum = max(tot_sum, cur_sum)
+        cur_max, glo_max = 0, nums[0]
+        cur_min, glo_min = 0, nums[0]
+        total = 0
+        for val in nums:
+            if cur_max + val > 0:
+                cur_max += val
+                glo_max = max(glo_max, cur_max)
             else:
-                cur_sum = 0
-                tot_sum = max(tot_sum, nums[i]) # could meet a less negative number
+                cur_max = 0
+                glo_max = max(glo_max, val)
+            if cur_min + val < 0:
+                cur_min += val
+                glo_min = min(glo_min, cur_min)
+            else:
+                cur_min = 0
+                glo_min = min(glo_min, val)
+            total += val
         
-        return tot_sum
+        # if all negatives
+        if glo_max < 0:
+            return glo_max
+        else:
+            return max((total - glo_min, glo_max))
