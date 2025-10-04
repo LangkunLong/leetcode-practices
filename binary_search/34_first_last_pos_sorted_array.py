1a53f44e23051f525bc982d4bc58b70c021c3990
Larry Long <Langkunlong@gmail.com>
2025-10-03 23:16:38 -0400

    just finding m does not mean we found lower/upper bound
    continue searching for boundary after finding target

--- binary_search/34_first_last_pos_sorted_array.py
+++ binary_search/34_first_last_pos_sorted_array.py
@@ -1,30 +1,26 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         # non-decreasing order => increasing with duplicates
-        # just call binary search twice, find position of target and target + 1 (start_pos of next bigger val) and - 1
-        sol = [-1, -1]
-        l, r = 0, len(nums) - 1
-        # find start pos    
-        while l <= r:
-            m = (l+r) // 2
-            if nums[m] == target:
-                sol[0] = m
-            elif target > nums[m]:
-                l = m + 1
-            else:
-                r = m - 1
-
-        if sol[0] == -1:
-            return sol
+        # just call binary search twice, find lower and upper bound
         
-        l, r = 0, len(nums) - 1
-        # find second   
-        while l <= r:
-            m = (l+r) // 2
-            if nums[m] == t:
-                sol[1] = m - 1
-            elif target > nums[m]:
-                l = m + 1
-            else:
-                r = m - 1
+        def helper(l, r, lowerBound):
+            i = -1
+            while l <= r:
+                m = (l + r) //2
+                if nums[m] == target:
+                    i = m
+                    # need to keep looking through left/right index to find lower/upper bound
+                    if lowerBound:
+                        r = m - 1
+                    else:
+                        l = m + 1
+                elif target > nums[m]:
+                    l = m + 1
+                else:
+                    r = m -1
+            return i
+        left = helper(0, len(nums)-1, True)
+        right = helper(0, len(nums)-1, False)
+
+        return [left, right]
         
