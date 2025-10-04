a5006cf7fa63626cf83957bc6a06d2e6665f3e53
Larry Long <Langkunlong@gmail.com>
2025-10-04 17:30:01 -0400

    update solution to use modified binary search instead of divide and conquer

--- binary_search/153_find_min_in_rotated_sorted_array.py
+++ binary_search/153_find_min_in_rotated_sorted_array.py
@@ -1,17 +1,22 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         # array is rotated left, find min element in logn time
-        # binary search problems: need to know what is our search space?
-        # are we searching left of mid, or right of mid?
-        res = [nums[0]] # set initial value 
+        # need to satisy nums[l] <= nums[m] <= nums[r] to see if we are in unrotated portion
+        l, r = 0, len(nums) - 1
+        res = [nums[0]]
 
         def helper(l,r):
             if l > r:
                 return 
             m = (l + r) // 2
             res[0] = min(res[0], nums[m])
-            l_min = helper(l, m-1)
-            r_min = helper(m+1, r)
+            if nums[l] <= nums[m] and nums[m] <= nums[r]:
+                helper(l, m-1)
+            else:
+                if nums[l] > nums[m]:
+                    helper(l, m-1)
+                else:
+                    helper(m+1, r)
+        helper(l, r)
         
-        helper(0, len(nums) - 1)
         return res[0]
