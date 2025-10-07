a9feb255d853fbf7e7f7cb681d636545b98f29ad
Larry Long <Langkunlong@gmail.com>
2025-10-07 11:06:54 -0400

    still TLE with randint() pivot

--- heap/215_k_largest_in_array.py
+++ heap/215_k_largest_in_array.py
@@ -1,19 +1,21 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        import random
         # no sorting, how do we rank elements? 
         # partition the array based on a pivot, use pointer p to track how many elemnts in the array are smaller than pivot
         # if have pointer p == N - k, then we have reached the spot with k's largest elemment
         # if nums[i] smaller than p, we swap positions with p, and increment p, 
         # if not, we don't swap, don't increment p but we still traverse the array (since not sorted can have other elements smaller)
         def partition(l, r):
-            m = (l + r) // 2
-            p, pivot = l, nums[m] # picked arbitrary pivot, can be any value
-            for i in range(l, r+1): # don't need to look at last value since that is the pivot
+            pivot_idx = random.randint(l, r)
+            nums[pivot_idx], nums[r] = nums[r], nums[pivot_idx]  # move pivot to end
+            p, pivot = l, nums[r]
+            for i in range(l, r): # don't need to look at last value since that is the pivot
                 if nums[i] <= pivot:
                     nums[i], nums[p] = nums[p], nums[i]
                     p += 1
             # have to swap pivot
-            nums[p], nums[m] = nums[m], nums[p]
+            nums[p], nums[r] = nums[r], nums[p]
             #print(p)
 
             if len(nums) - k > p: return partition(p+1, r)
@@ -22,4 +24,3 @@ class Solution:
                 #print(nums[p])
                 return nums[p]
         return partition(0, len(nums)-1)
-
