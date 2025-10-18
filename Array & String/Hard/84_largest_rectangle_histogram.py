7e1942e087cf16dc674221e932f94b1adcec7d7c
Larry Long <Langkunlong@gmail.com>
2025-10-18 19:00:03 -0400

    initial approach, 77/99

--- Array & String/Hard/84_largest_rectangle_histogram.py
+++ Array & String/Hard/84_largest_rectangle_histogram.py
@@ -5,3 +5,29 @@ class Solution:
         # we pop the stack if we encounter the smaller height, and push the new element with a new starting index (extend starting index to the left)
         # in the stack, keep track of its height, and maximum starting position
         # extend the starting position by seeing how many elements we pop from the stack (how many are bigger than cur height)
+
+        maxArea = 1
+        stack = []
+        stack.append( (0, heights[0]) )
+        for i in range(1, len(heights)):
+            num_pop = 0
+            while stack and heights[i] < stack[-1][1]:
+                start, height = stack[-1]
+                curArea = (i - start) * height
+                maxArea = max(maxArea, curArea)
+                stack.pop()
+                num_pop += 1
+            stack.append( (i - num_pop, heights[i]))
+        
+        # if still left over
+        while stack:
+            start, height = stack.pop()
+            curArea = (len(heights) - start) * height
+            maxArea = max(maxArea, curArea)
+        
+        return maxArea
+            
+
+
+            
+
