e7bbd6ce50a57432a9b59609c0a36861e5df1355
Larry Long <Langkunlong@gmail.com>
2025-10-21 19:32:27 -0400

    can't assume array same length

--- heap/373_k_pairs_w_smallest sum.py
+++ heap/373_k_pairs_w_smallest sum.py
@@ -1,1 +1,15 @@
 # why can't solve with 2 pointers??
+# can't assume array are same lengths
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        # 2 pointer approach
+        l1, l2 = 0, 0
+        res = []
+        for i in range(k):
+            if nums1[l1] < nums2[l2]:
+                res.append([nums1[l1], nums2[l2]])
+                l2 += 1
+            else:
+                res.append([nums1[l1], nums2[l2]])
+                l1 += 1
+        return res
