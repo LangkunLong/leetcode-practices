877e4a8e34b5af244004e4f7411b3b6061b9aa45
Larry Long <Langkunlong@gmail.com>
2025-10-21 20:13:42 -0400

    using priority queue (bfs approach) with visited set

--- heap/373_k_pairs_w_smallest sum.py
+++ heap/373_k_pairs_w_smallest sum.py
@@ -1,20 +1,23 @@
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
         # need to compare sums, since given non-decreasing, we can explore next index elements
+        # need to use heap, cannot use 2 pointers because will have to iteratively increase / decrease pointers, 
+        # need to check all possible surrounding combinations, since given in order, can check next index 
+        # similar to BFS approach: using heap (priority queue) and visited set
         import heapq
         min_sum = []
         res = []
         heapq.heappush(min_sum, (nums1[0]+nums2[0], 0, 0))
-        #visited = set()
+        visited = set()
 
         for i in range(k):
             p_sum, n1, n2 = heapq.heappop(min_sum)
-            #visited.add((n1,n2))
+            visited.add((n1,n2))
             res.append([nums1[n1], nums2[n2]])
-            if n1 + 1 < len(nums1): #and (n1 + 1, n2) not in visited:
+            if n1 + 1 < len(nums1) and (n1 + 1, n2) not in visited:
                 heapq.heappush( min_sum, ( nums1[n1+1]+nums2[n2], n1 + 1, n2))
-                #visited.add((n1+1, n2))
-            if n2 +1 < len(nums2): #and (n1, n2+1) not in visited:
+                visited.add((n1+1, n2))
+            if n2 +1 < len(nums2) and (n1, n2+1) not in visited:
                 heapq.heappush( min_sum, ( nums1[n1]+nums2[n2+1], n1, n2+1))
-                #visited.add((n1, n2+1))
+                visited.add((n1, n2+1))
         return res
