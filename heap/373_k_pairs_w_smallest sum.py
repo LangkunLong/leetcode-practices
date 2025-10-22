694a8226e640fbaf87cf62d7a0b51ae0c250712a
Larry Long <Langkunlong@gmail.com>
2025-10-21 20:11:16 -0400

    testing heap approach without visited

--- heap/373_k_pairs_w_smallest sum.py
+++ heap/373_k_pairs_w_smallest sum.py
@@ -1,24 +1,20 @@
-# why can't solve with 2 pointers??
-# can't assume array are same lengths
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        # 2 pointer approach
-        l1, l2 = 0, 0
+        # need to compare sums, since given non-decreasing, we can explore next index elements
+        import heapq
+        min_sum = []
         res = []
+        heapq.heappush(min_sum, (nums1[0]+nums2[0], 0, 0))
+        #visited = set()
+
         for i in range(k):
-            if l1 < len(nums1) and l2 < len(nums2):
-                res.append([nums1[l1], nums2[l2]])
-                if nums1[l1] < nums2[l2]:
-                    l2 += 1
-                else:
-                    l1 += 1
-            else:
-                if l1 == len(nums1):
-                    l2 += 1
-                    l1 = 0
-                else:
-                    l1 += 1
-                    l2 = 0
-
+            p_sum, n1, n2 = heapq.heappop(min_sum)
+            #visited.add((n1,n2))
+            res.append([nums1[n1], nums2[n2]])
+            if n1 + 1 < len(nums1): #and (n1 + 1, n2) not in visited:
+                heapq.heappush( min_sum, ( nums1[n1+1]+nums2[n2], n1 + 1, n2))
+                #visited.add((n1+1, n2))
+            if n2 +1 < len(nums2): #and (n1, n2+1) not in visited:
+                heapq.heappush( min_sum, ( nums1[n1]+nums2[n2+1], n1, n2+1))
+                #visited.add((n1, n2+1))
         return res
-
