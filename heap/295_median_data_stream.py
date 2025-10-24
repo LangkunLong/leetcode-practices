bb25e8c04a588ec9c9a0943633527d8b41543446
Larry Long <Langkunlong@gmail.com>
2025-10-24 17:37:18 -0400

    solved with simple sortedList(), but how to solve without?

--- heap/295_median_data_stream.py
+++ heap/295_median_data_stream.py
@@ -1,1 +1,24 @@
 # can be directly solved with python sortedList, or maps (trees) in c++
+class MedianFinder:
+    # using sorted list, o(logn) index, insert, remove
+    from sortedcontainers import SortedList
+
+    def __init__(self):
+        self.stream = SortedList()
+
+    def addNum(self, num: int) -> None:
+        self.stream.add(num)
+
+    def findMedian(self) -> float:
+        n = len(self.stream)
+        if n % 2 == 0:
+            return (self.stream[n//2 - 1] + self.stream[n//2]) / 2
+        else:
+            return self.stream[n // 2]
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
