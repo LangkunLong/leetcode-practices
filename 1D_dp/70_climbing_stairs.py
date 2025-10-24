718a2e3fb7e5ca294152175b84cfd4d85f47e7a9
Larry Long <Langkunlong@gmail.com>
2025-10-24 19:43:14 -0400

    memoized solution

--- 1D_dp/70_climbing_stairs.py
+++ 1D_dp/70_climbing_stairs.py
@@ -3,3 +3,23 @@
 # don't need to generate all combinations, at each step, keep all the ways to go to step -2, and all the ways to go to step -1, and add them together
 # use memorization to store ways to climb at each step; 8 -> 6, 7; 6 -> 4, 5; 7 -> 5,6; 4 -> 2 (return), 3; 3->1(return), 2(return), store 3
 # 4 -> 2 (return), 3(return, since we stored it), so 4 returns; 5-> 3(return), 4(return); 6-> 4(return), 5(return) ....
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        mem_dict = dict()
+        mem_dict[1] = 1
+        mem_dict[2] = 2
+        def helper(n):
+            if n == 1 or n == 2:
+                return mem_dict[n]
+            elif n > 2:
+                if n-2 not in mem_dict or n-1 not in mem_dict:
+                    mem_dict[n-2] = helper(n-2)
+                    mem_dict[n-1] = helper(n-1)
+                mem_dict[n] = mem_dict[n-2] + mem_dict[n-1]
+                return mem_dict[n]
+
+        helper(n)
+        return mem_dict[n]
+
+            
