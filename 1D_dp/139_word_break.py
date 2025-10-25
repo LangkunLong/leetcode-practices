7daacaaf8d3986861e95a822f744e36aa8c74b96
Larry Long <Langkunlong@gmail.com>
2025-10-25 19:06:19 -0400

    work backwards from base case using bottom up approach

--- 1D_dp/139_word_break.py
+++ 1D_dp/139_word_break.py
@@ -1,13 +1,5 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # use start and end to mark subwindwow, if subwindow ever forms a word, update status to true
-        # after identifying to true, move subwindow to next available window
-        l, r = 0, 0
-        status = False
-        for char in s:
-            if s[l:r] in wordDict:
-                l += 1
-                r += 1
-                status = True
-            else:
-                r += 1
+        # decision tree is based on available words in wordDict\
+        # base case is when reading position len(s), means we have matched all words including the last element
+        # starting from the base case len(s), work backwards
