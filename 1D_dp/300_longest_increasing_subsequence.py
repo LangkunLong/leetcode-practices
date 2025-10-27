4154f53396073453e154775a9bf1ac180496e6da
Larry Long <Langkunlong@gmail.com>
2025-10-27 19:37:15 -0400

    using bottom up O(n^2) approach; cannot sort or else lose original subsequence ordering

--- 1D_dp/300_longest_increasing_subsequence.py
+++ 1D_dp/300_longest_increasing_subsequence.py
@@ -1,30 +1,15 @@
-# initial approach using bottom up dp
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # at each position, if next element is smaller, we accept next element's longest subsequence, because it is for sure
-        # longer then starting at current position; if current value is smaller, start a new subsequence and find the biggest 
-        # element it is smaller to in the preceding array 
-        dp = [0] * (len(nums))
-        dp[len(nums)-1] = 1
-        ind_mapper = dict()
-        
-        for idx, num in enumerate(nums):
-            ind_mapper[num] = idx
-        
-        for i in range(len(nums)-2, -1, -1):
-            if nums[i] > nums[i+1]:
-                dp[i] = dp[i-1]
-            else:
-                smallest_idx = self.bs_helper(nums[i+1:], nums[i])
-                dp[i] = max(dp[i], 1 + dp[smallest_idx])
-        return dp[0]
+        # base case is last index, can form a subsequence of 1
+        # go from last index to first, check all preceding indices, see if we can attach current subsequence to previous
+        # subsequence only if current value is smaller
+        # dp[0] represent the longest increasing subsequence starting at index 0, cannot just assume dp[0] is the longest
+        # need to return max(dp)
 
+        LIS = [1] * len(nums)
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(i, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+        return max(LIS)
 
-    def bs_helper(self, nums, target):
-        smallest_val = float('inf')
-        for idx, n in enumerate(nums):
-            if target < n:
-                if n < smallest_val:
-                    smallest_val = min(smallest_val, n)
-
-        return ind_mapper[smallest_val] 
