862d050acce11fffe2fc450dc307565e0174be72
Larry Long <Langkunlong@gmail.com>
2025-10-28 19:48:15 -0400

    bottom up using O(n^2) memory; how to solve using O(n)?

--- 1D_dp/120_triangle.py
+++ 1D_dp/120_triangle.py
@@ -2,6 +2,7 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         # base case, reaching the leaf node
         # each time we move down the triangle there are 2 choices, lowest path sum at i, or lowest path sum at i + 1
+        # using O(n^2) memory, how to solve using O(n) space?
         dp = [[num for num in layer] for layer in triangle]
         #print(dp)
         for i in range(len(triangle)-2, -1, -1):
