97bf2643d8b3f76edf030db9e9f62cbc8a69a94d
Larry Long <Langkunlong@gmail.com>
2025-10-28 19:47:20 -0400

    solved using bottom up approach

--- 1D_dp/120_triangle.py
+++ 1D_dp/120_triangle.py
@@ -2,9 +2,10 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         # base case, reaching the leaf node
         # each time we move down the triangle there are 2 choices, lowest path sum at i, or lowest path sum at i + 1
-        # or at each layer we just take the minimum
-        res = 0
-        for layer in triangle:
-            min_cost = min(layer)
-            res += min_cost
-        return res
+        dp = [[num for num in layer] for layer in triangle]
+        #print(dp)
+        for i in range(len(triangle)-2, -1, -1):
+            for j in range(len(triangle[i])):
+                dp[i][j] += min(dp[i+1][j], dp[i+1][j+1])
+        print(dp)
+        return dp[0][0]
