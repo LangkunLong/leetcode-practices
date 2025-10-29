2dda99ab9d84af22383fca9cecbd4626ae3a9b5a
larry <langkunlong@gmail.com>
2025-10-28 20:17:59 -0400

    only need info from last row

--- 1D_dp/120_triangle.py
+++ 1D_dp/120_triangle.py
@@ -12,3 +12,10 @@ class Solution:
         return dp[0][0]
 
         # O(n) memory, only need to keep information from the previous row:
+        dp = triangle[-1][:]  # start from the last row
+        
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
+        
+        return dp[0]
