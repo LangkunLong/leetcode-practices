77a665425e8cd05742b5870196d815639a8e2ada
Larry Long <Langkunlong@gmail.com>
2025-11-06 18:49:30 -0500

    solve with caching since unique path at [i][j] is unique paths at [i+1][j] and [i][j+1]

--- Multi_DP/63_unique_paths_2.py
+++ Multi_DP/63_unique_paths_2.py
@@ -1,13 +1,15 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        # recursive dfs?
+        # recursive dfs, add cachingL unique paths at position [i] is unique paths from position [i+1] and [j+1]
         row, col = len(obstacleGrid), len(obstacleGrid[0])
+        dp = {(row-1, col-1):1}
         def dfs(r, c):
             if r == row or c == col or obstacleGrid[r][c] == 1:
                 return 0
-            if r == row -1 and c == col -1:
-                return 1
+            if (r, c) in dp:
+                return dp[(r,c)]
 
-            return dfs(r+1, c) + dfs(r, c+1)
+            dp[(r,c)] = dfs(r+1, c) + dfs(r, c+1)
+            return dp[(r,c)]
         
         return dfs(0,0)
