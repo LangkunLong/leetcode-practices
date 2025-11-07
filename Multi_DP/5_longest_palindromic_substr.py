e51bc43be9d582b4135f585698d2ab4013bc5b03
Larry Long <Langkunlong@gmail.com>
2025-11-06 20:15:34 -0500

    better approach for evaluating palindrome but still don't feel like its a dp solution

--- Multi_DP/5_longest_palindromic_substr.py
+++ Multi_DP/5_longest_palindromic_substr.py
@@ -4,22 +4,24 @@ class Solution:
         # at index 2: {start substring with b: 1
         #              attach to 'a', 'ab' : 0
         #              attach to 'ba', 'bab': 3}
-        if len(s) == 1:
-            return s
-
         longest = ""
-        for i in range(len(s)-1, -1 , -1):
-            for j in range(i, len(s)):
-                if self.helper(s[i:j+1]):
-                    if len(s[i:j+1]) > len(longest):
-                        longest = s[i:j+1]
+        length = 0
+        for i in range(len(s)):
+            # even
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if len(s[l:r+1]) > length:
+                    longest = s[l:r+1]
+                    length = r-l + 1
+                l -= 1
+                r += 1
+            # odd
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if len(s[l:r+1]) > length:
+                    longest = s[l:r+1]
+                    length = r-l + 1
+                l -= 1
+                r += 1
         return longest
-    
-    def helper(self, s):
-        l, r = 0, len(s) -1
-        while l <= r:
-            if s[l] != s[r]:
-                return False
-            l += 1
-            r -= 1
-        return True
+                
