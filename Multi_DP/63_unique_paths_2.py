fcd6855ee4c2d3a99a509130b4865a430cab33bf
Larry Long <Langkunlong@gmail.com>
2025-11-06 19:17:02 -0500

    adding in O(n) space solution

--- Multi_DP/63_unique_paths_2.py
+++ Multi_DP/63_unique_paths_2.py
@@ -13,3 +13,20 @@ class Solution:
             return dp[(r,c)]
         
         return dfs(0,0)
+
+        class Solution:
+    
+        # optimal O(N) approach
+        # only need 1 row for memory
+        row, col = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [0] * col
+        dp[col - 1] = 1
+        
+        for r in range(row-1, -1, -1):
+            for c in range(col-1, -1, -1):
+                if obstacleGrid[r][c]:
+                    dp[c] = 0
+                elif c + 1 < col:
+                    dp[c] = dp[c] + dp[c+1]
+        return dp[0]
+
