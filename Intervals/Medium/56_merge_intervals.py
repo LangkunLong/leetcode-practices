c228e4782164c459ebd913e55168c26eb4f1c701
Larry Long <Langkunlong@gmail.com>
2025-11-18 22:42:07 -0500

    Update 56_merge_intervals.py

--- Intervals/Medium/56_merge_intervals.py
+++ Intervals/Medium/56_merge_intervals.py
@@ -1,5 +1,8 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # re-doing this question
+        # re-doing this question, first sort using start indices, so we have intervals in increasing order
+        # if the start index of second interval <= end index of first interval, update end interval to max(e1, e2)
+
+
             
                 
