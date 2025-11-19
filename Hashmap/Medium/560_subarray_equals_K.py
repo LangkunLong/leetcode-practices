e31713b7a8179e2684d4cc29941e39849b3ed747
Larry Long <Langkunlong@gmail.com>
2025-11-19 17:35:30 -0500

    prefix sum solution
    at each position, how many subarrays we can get that sum up to k? we can remove a prefix sum of  (cur_sum - k) to get to k, and we have a count of up until this position in the array, how many unique prefix sums of (cur_sum -k)

--- Hashmap/Medium/560_subarray_equals_K.py
+++ Hashmap/Medium/560_subarray_equals_K.py
@@ -1,3 +1,18 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-      # init
+        # cannot use sliding window because array contains negative numbers
+        # simply adding/ removing does not necessarily mean we are increasing / decreasing the size
+        prefix_map = {0:1}
+        cur_sum = 0
+        res = 0
+        for num in nums:
+            cur_sum += num
+            # at current position, how mnay subarrays we can get that sum up to k? we can remove a prefix sum of
+            # (cur_sum -k) to sum to k, and we have a count of that prefix sum
+            if cur_sum - k in prefix_map:
+                res += prefix_map[cur_sum-k]
+            if cur_sum not in prefix_map:
+                prefix_map[cur_sum] = 1
+            else:
+                prefix_map[cur_sum] += 1
+        return res
