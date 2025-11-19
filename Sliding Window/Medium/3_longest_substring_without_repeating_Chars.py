589ae3ea78e5ee9e0845a5301cd0eac74efc06a4
Larry Long <Langkunlong@gmail.com>
2025-11-18 22:21:29 -0500

    better understanding by listing explicit steps

--- Sliding Window/Medium/3_longest_substring_without_repeating_Chars.py
+++ Sliding Window/Medium/3_longest_substring_without_repeating_Chars.py
@@ -1,1 +1,19 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+        duplicate = set()
+        left = 0
+        longest = 1
+        for right in range(len(s)):
+            if s[right] not in duplicate:
+                longest = max(longest, right - left + 1)
+                duplicate.add(s[right])
+            else:
+                while s[right] in duplicate:
+                    duplicate.remove(s[left])
+                    left += 1
+                duplicate.add(s[right])
+
+        return longest
 
