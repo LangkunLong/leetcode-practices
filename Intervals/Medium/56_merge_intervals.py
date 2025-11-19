aca3712caae309113b6d0e7c9188ec34e3bc0f53
Larry Long <Langkunlong@gmail.com>
2025-11-18 22:42:22 -0500

    re-doing this question

--- Intervals/Medium/56_merge_intervals.py
+++ Intervals/Medium/56_merge_intervals.py
@@ -3,6 +3,18 @@ class Solution:
         # re-doing this question, first sort using start indices, so we have intervals in increasing order
         # if the start index of second interval <= end index of first interval, update end interval to max(e1, e2)
 
+        intervals = sorted(intervals, key=lambda x:x[0])
+        res = []
+        for interval in intervals:
+            if not res:
+                res.append(interval)
+            start, end = interval[0], interval[1]
+            cur_end = res[-1][1]
 
+            if start <= cur_end:
+                res[-1][1] = max(res[-1][1], end)
+            else:
+                res.append(interval)
+        return res
             
                 
